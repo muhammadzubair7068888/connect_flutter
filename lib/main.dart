6d3467fcb4dc3f115ce6013fc2b0989cf1af3e0c
Hamza-Iftikhar12 <hamzaiftekhar381@gmail.com>
2022-06-25 21:29:07 +0500

    by hamza

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,27 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   final storage = const FlutterSecureStorage();
-  late String token;
-  late String role;
+  String? token;
+  String? role;
   @override
   void initState() {
     super.initState();
-    token = storage.read(key: "token").toString();
-    role = storage.read(key: "role").toString();
-    print("welcome");
+    Future<void> _readToken() async {
+      final t = await storage.read(key: "token");
+      setState(() {
+        token = t;
+      });
+    }
+
+    Future<void> _readRole() async {
+      final r = await storage.read(key: "role");
+      setState(() {
+        role = r;
+      });
+    }
+
+    _readToken();
+    _readRole();
   }
 
   @override
@@ -45,9 +58,9 @@ class _MyAppState extends State<MyApp> {
             ),
       ),
       // ignore: unnecessary_null_comparison
-      home: token == null || token == "" || token.isEmpty
+      home: token == null || token == "" || token!.isEmpty
           ? const WelcomeScreen()
-          : BottomNavBar(role: role),
+          : BottomNavBar(role: role!),
       builder: EasyLoading.init(),
     );
   }
