90a96b3961187546b1da373aa5b8297259ddcecc
muhammadzubair7068888 <muhammadzubair7068888@gmail.com>
2022-09-08 12:26:46 +0500

    by hamza

--- lib/Globals/globals.dart
+++ lib/Globals/globals.dart
@@ -1,11 +1,11 @@
-const String apiURL = "http://192.168.10.3/connect_laravel/public/api/";
-const String downloadUrl =
-    "http://192.168.10.3/connect_laravel/public/uploads/";
-const String publicUrl = "http://192.168.10.3/connect_laravel/public/";
-const Map<String, String> headers = {
-  'Content-Type': 'application/json',
-  'Charset': 'utf-8'
-};
-// const String apiURL = "http://connect.connected-performance.com/api/";
-// const String downloadUrl = "http://connect.connected-performance.com/uploads/";
-// const String publicUrl = "http://connect.connected-performance.com/";
+// const String apiURL = "http://192.168.10.3/connect_laravel/public/api/";
+// const String downloadUrl =
+//     "http://192.168.10.3/connect_laravel/public/uploads/";
+// const String publicUrl = "http://192.168.10.3/connect_laravel/public/";
+// const Map<String, String> headers = {
+//   'Content-Type': 'application/json',
+//   'Charset': 'utf-8'
+// };
+const String apiURL = "http://connect.connected-performance.com/api/";
+const String downloadUrl = "http://connect.connected-performance.com/uploads/";
+const String publicUrl = "http://connect.connected-performance.com/";
